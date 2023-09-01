9a7e82b6806c15b114b9b395380f2fcf3395dd2d
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-01 20:33:52 +0100

    feat: initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
+
+use clap::Parser;
+
+fn publicise(mut r: impl io::Read) -> anyhow::Result<String> {
+    let mut s = String::new();
+    r.read_to_string(&mut s)?;
+    let f = syn::parse_file(&s)?;
+    Ok(prettyplease::unparse(&f))
+}
+
+struct MakePub;
+
+impl syn::visit_mut::VisitMut for MakePub {
+    fn visit_visibility_mut(&mut self, i: &mut syn::Visibility) {
+        *i = syn::Visibility::Public(syn::token::Pub::default())
+    }
+}
+
+#[derive(Parser)]
+struct Args {
+    #[arg(name("FILE"))]
+    files: Vec<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let Args { files } = Args::parse();
+    if files.is_empty() || (files.len() == 1 && files[0] == Path::new("-")) {
+        eprintln!("publicise: <stdin>");
+        println!("{}", publicise(io::stdin())?);
+    } else {
+        for file in files {
+            println!("publicise: {}", file.display());
+            let s = publicise(fs::File::open(&file)?)?; // mind your drop guards!
+            fs::write(file, s)?;
+        }
+    }
+    Ok(())
 }
